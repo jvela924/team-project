cf96a83a2e23dc418baca7e65f746063f4d6e4a4
aneme1038 <thriftman25@gmail.com>
2019-08-15 20:51:19 -0400

    adjusted form in index.html

--- public/js/app.js
+++ public/js/app.js
@@ -2,132 +2,130 @@ const app = angular.module('MyApp', []);
 
 app.controller('disqoverController', ['$http', function($http){
 
-const controller = this;
+  const controller = this;
 
-this.logOut = function(){
+  this.logOut = function(){
     $http({
         method:'DELETE',
         url:'/sessions'
     }).then(function(response){
         console.log(response);
-        controller.loggedInUsername = null;
+        controller.loggedInUsername = undefined;
     }, function(error){
         console.log(error);
     });
-}
+  }
 
-this.createUser = function(){
-  $http({
-      method:'POST',
-      url: '/users',
-      data: {
-          username: this.username,
-          password: this.password
-      }
-  }).then(function(response){
-      console.log(response);
-      controller.loggedInUsername = null;
-  }, function(){
-      console.log("error");
-  });
-}
-
-this.logIn = function(){
+  this.createUser = function(){
     $http({
         method:'POST',
-        url:'/sessions',
+        url: '/users',
         data: {
             username: this.username,
             password: this.password
         }
     }).then(function(response){
         console.log(response);
+        controller.loggedInUsername = null;
+    }, function(){
+        console.log("error");
+    });
+  }
+
+  this.logIn = function(){
+    $http({
+        method:'POST',
+        url:'/sessions',
+        data: {
+          username: this.username,
+          password: this.password
+        }
+    }).then(function(response){
+        console.log(response);
         controller.login = null;
         controller.credentials = null;
-        controller.loggedInUsername = response.date.userData.username
-        controller.goAuthorization(); //add this
+        controller.loggedInUsername = response.data.userData.username
+        // controller.goAuthorization(); //add this
     }, function(error){
         console.log(error);
     })
-}
+  }
 
-this.goAuthorization = function(){
-    $http({
-        method:'GET',
-        url: '/app'
-    }).then(function(response){
-        controller.loggedInUsername = response.data.username; //change this
-    }, function(){
-        console.log('error');
-    });
-}
+  //this callback might be used if we add a different route for user login
+  // this.goAuthorization = function(){
+  //     $http({
+  //         method:'GET',
+  //         url: '/app'
+  //     }).then(function(response){
+  //         controller.loggedInUsername = response.data.username; //change this
+  //     }, function(){
+  //         console.log('error');
+  //     });
+  // }
 
-this.deleteItem = function(item){
-      console.log('hello');
+  this.deleteItem = function(item){
       $http({
           method:'DELETE',
           url: '/disqover/'+ item._id,
       }).then(
+        function(response){
+            controller.getItem();
+      })
+    }
+
+  this.editItem = function(item){
+      $http({
+          method:'PUT',
+          url: '/disqover/'+ item._id,
+          data: {
+            name: this.name,
+            image: this.image,
+            age: this.age,
+            bio: this.bio,
+            fav_artists: this.fav_artists,
+            fav_movies: this.fav_movies,
+            username: this.username,
+          }
+      }).then(
           function(response){
               controller.getItem();
+              controller.indexOfEditFormToShow = null;
+          },
+          function(error){
 
           }
       );
   }
 
-this.editItem = function(item){
-    $http({
-        method:'PUT',
-        url: '/disqover/'+ item._id,
-        data: {
-          name: this.name,
-          image: this.image,
-          age: this.age,
-          bio: this.bio,
-          fav_artists: this.fav_artists,
-          fav_movies: this.fav_movies,
-          username: this.username,
-        }
-    }).then(
-        function(response){
-            controller.getItem();
-            controller.indexOfEditFormToShow = null;
-        },
-        function(error){
-
-        }
-    );
-}
-
-this.getItem = function(){
-    $http({
-      method:'GET',
-      url: '/disqover/',
-    }).then(function(response){
-      controller.item = response.data;
-      console.log(response.data);
-    });
-  };
-
-this.createItem = function(){
+  this.getItem = function(){
       $http({
-           method:'POST',
-           url: '/disqover/',
-           data: {
-             name: this.name,
-             image: this.image,
-             age: this.age,
-             bio: this.bio,
-             fav_artists: this.fav_artists,
-             fav_movies: this.fav_movies,
-             username: this.username,
-              }
-       }).then(function(){
+        method:'GET',
+        url: '/disqover/',
+      }).then(function(response){
+        controller.item = response.data;
+        console.log(response.data);
+      });
+    };
 
-    controller.getItem();
-  });
-    }
+  this.createItem = function(){
+        $http({
+             method:'POST',
+             url: '/disqover/',
+             data: {
+               name: this.name,
+               image: this.image,
+               age: this.age,
+               bio: this.bio,
+               fav_artists: this.fav_artists,
+               fav_movies: this.fav_movies,
+               username: this.username,
+                }
+         }).then(function(){
 
-this.getItem();
+      controller.getItem();
+    });
+      }
+
+  this.getItem();
 
 }]);
